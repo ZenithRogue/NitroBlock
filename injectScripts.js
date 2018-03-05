12189ce84a1d69d1079f947ec246a83296c6682c
Bill Shoener <nitrocipher@gmail.com>
2018-03-05 10:09:25 -0600

    Delete injectScripts.js

--- injectScripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var extensionURL = chrome.extension.getURL("MainScript.js");
-	var script = document.createElement("script");
-	script.innerHTML = "try {$.when(window.SWFready).done(function() { try { if (JSsetProjectStats) { var bak = JSsetProjectStats; JSsetProjectStats = function(a,b,c,d) { var script = document.createElement(\"script\");script.src = \"" + extensionURL + "\"; document.body.appendChild(script); return bak(a,b,c,d); }}} catch (e) {}});} catch (e) {}";
-	document.body.appendChild(script);
-});
